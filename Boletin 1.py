923a24892aa6175b592f8392181163755c0de3b1
Pereira <mpereirafernandez@danielcastelao.org>
2025-10-16 12:38:25 +0200

    Añadido boletin 1

---
+++ Boletin 1.py
@@ -0,0 +1,104 @@
+#Ejercicio 1
+#a)
+print(((3 + 2) % 2 - 15) / 2 * 5)
+#b)
+print((6 + (6) / 7) + 35 / 2 -8 * 5 / 4 * 2 )
+#c)
+print(3 + 6 * 14 % 3)
+#d)
+print(8 + 7 * 3 + 4 * 6 /2 % 4)
+#e)
+print(27 % 4 +15 / 4)
+#f)
+print(37 / 42  - 2)
+#g)
+print(9 * 2 / 3 * 25 * 3)
+#h)
+print((7 * 3 - 4 * 4) * 2 / 4 * 2)
+
+#Ejercicio 2
+Salto- mortal = 0
+#Invalido
+salto_mortal = 0
+#Valido
+salto + mortal = 0
+#Invalido
+2salto = 0
+#Invalido
+“salto” = 0
+#Invalido
+cantidade total = 0
+#Invalido
+cant_total = 0
+#Valido
+cant5 = 0
+#Valido
+cantidadeTotal = 0
+#Valido
+
+#Ejercicio 3
+#a)
+m = 1
+n = 2
+print((m+n)/n)
+#b)
+p = 3
+r = 4
+s= 5
+print((m+n)*s / (p-r)*p)
+#c)
+q = 6
+print((m+4) / (p-q))
+#d)
+c = 7
+t = 8
+print(c*r*t/100)
+#f) rint((m/n) * (p+q))
+print((m/n) * (p+q))
+#e)
+print((m+n) / (p+(q/r)))
+#g)
+i = 9
+print((n*((1+i)**t) * i)/(((1+i)**t)-1))
+
+#Ejercicio 4
+ej1 = True
+ej2 = False
+print(ej1 and ej1)
+print(not ej2)
+print((ej1 and ej1) or ej2)
+print((ej2 or ej2) and ej2)
+print((not(ej1 and ej2)))
+print('12'+'12')
+print('34'+ '43')
+
+#Ejercicio 5
+i2 = 1
+j2 = 0
+k2 = 1
+print(i2 + k2 <= j2 - k2 * 3 and k2 >= 2)
+i3 = 3
+j3 = 2
+k3 = -1
+print( i3 == 3 or j3 <= 2 and k3 > 0)
+#c)
+tipo = 10
+rede = 7.5
+print(tipo<rede + 1.5)
+#d)
+ano = 1993
+print(ano % 400 == 0)
+#g)
+a = 2
+b = 5
+c = 6
+d = 10
+print(a >= b or a >= c and a <d)
+#h)
+print(a + b < c and a + c < d or 2 * a < a + b)
+#i)
+print(not (a * b < d) and not (a * b < c) or b + c <= d)
+#e)
+print(3 == 2 or 5 > 1 + 1 )
+#f)
+print(5 - 2 > 4 and not(0,5 == 1 / 5))
